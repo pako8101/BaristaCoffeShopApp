d1a35aee1f5d98e49140de84d8a560edc6efd382
pako8101 <pako810129@gmail.com>
2024-02-20 16:16:41 +0200

    initial

---
+++ src/test/java/com/example/baristacoffeshopapp/BaristaCoffeShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baristacoffeshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaristaCoffeShopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
