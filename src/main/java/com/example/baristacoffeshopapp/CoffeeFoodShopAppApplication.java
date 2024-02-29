cd3a9f92662949b7b3ab25f2563796269945d356
pako8101 <pako810129@gmail.com>
2024-02-29 19:11:34 +0200

    front css rearrange

---
+++ src/main/java/com/example/baristacoffeshopapp/CoffeeFoodShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.baristacoffeshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeFoodShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeFoodShopAppApplication.class, args);
+	}
+
+}
