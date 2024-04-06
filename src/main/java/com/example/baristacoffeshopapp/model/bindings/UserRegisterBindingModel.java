c56e06df92bc598d837cb6893b50d5838b8c8626
pako8101 <pako810129@gmail.com>
2024-04-06 12:21:27 +0300

    html changes

--- src/main/java/com/example/baristacoffeshopapp/model/bindings/UserRegisterBindingModel.java
+++ src/main/java/com/example/baristacoffeshopapp/model/bindings/UserRegisterBindingModel.java
@@ -2,26 +2,32 @@ package com.example.baristacoffeshopapp.model.bindings;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserRegisterBindingModel {
-@Size(min = 5,max = 20)
+    @Length(min = 3, max = 20, message = "The username must be between 3 and 20 characters")
+    @NotBlank(message = "The field must not be empty")
     private String username;
-
+    @NotBlank(message = "First name cannot be empty!")
     private String firstName;
-    @Size(min = 5,max = 20)
+    @Size(min = 2, max = 20)
     private String lastName;
-    @Email
+    @Email(message = "Invalid email")
+    @NotBlank(message = "The field must not be empty")
     private String email;
-    @Size(min = 3)
+    @Length(min = 3, max = 20, message = "The password must be between 3 and 20 characters")
+    @NotBlank(message = "The field must not be empty")
     private String password;
-    @Size(min = 3)
+    @Length(min = 3, max = 20, message = "The password length must be between 3 and 20 characters")
+    @NotBlank
     private String confirmPassword;
 
 }
